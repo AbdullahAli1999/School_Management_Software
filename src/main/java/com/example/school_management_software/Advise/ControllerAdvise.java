c63b3b1f637bfec25eb86022d5bd9b3cd7a8463f
AbdullahAli1999 <iifamxx@gmail.com>
2025-04-17 11:34:31 +0300

    i forgot to add

--- src/main/java/com/example/school_management_software/Advise/ControllerAdvise.java
+++ src/main/java/com/example/school_management_software/Advise/ControllerAdvise.java
@@ -9,11 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
-
+@ControllerAdvice
 public class ControllerAdvise {
     // Our Exception
     @ExceptionHandler(value = ApiException.class)
